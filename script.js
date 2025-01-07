99b48845c366da45bc6c44b968fe14f2c0c0c417
darkustarku <ahmed.abdelelamin@gmail.com>
2025-01-06 22:03:45 -0500

    adds playround and playgame funcs

--- script.js
+++ script.js
@@ -9,4 +9,97 @@ function getComputerChoice() {
     return choices[pick];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice () {
+    let choice = prompt("what will you throw?");
+    return choice.toLowerCase();
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        human = humanChoice.toLowerCase();
+        computer = computerChoice.toLowerCase();
+    
+        console.log(human, computer);
+    
+        if (human == computer) {
+            console.log("It's a draw!");
+            return;
+        }
+    
+        else if (human == "rock") {
+            if (computer == "scissors") {
+                console.log("You won the round!");
+                humanScore += 1;
+                return;
+            }
+    
+            if (computer == "paper") {
+                console.log("You lost the round :(");
+                computerScore += 1;
+                return;
+            }
+    
+        }
+    
+        else if (human == "paper") {
+            if (computer == "scissors") {
+                console.log("You lost the round :(");
+                computerScore += 1;
+                return;
+            }
+    
+            if (computer == "rock") {
+                console.log("You won the round!");
+                humanScore += 1;
+                return;
+            }
+    
+        }
+    
+        else if (human == "scissors") {
+            if (computer == "rock") {
+                console.log("You lost the round :(");
+                computerScore += 1;
+                return;
+            }
+    
+            if (computer == "paper") {
+                console.log("You won the round!");
+                humanScore += 1;
+                return;
+            }
+    
+        }
+        
+        
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(humanScore, computerScore);
+    }
+
+    if (humanScore == computerScore) {
+        console.log("You drew to the computer!");
+    }
+
+    else if (humanScore > computerScore) {
+        console.log("YOU WON THE GAMEEE!!! :)))")
+    }
+
+    else {
+        console.log("..you lost... :/")
+    }
+
+}
+
+
+playGame();
